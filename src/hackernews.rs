3d690644274035f41ef7cf1f7bfbea7402017786
jwh1550 <j0861550@gmail.com>
2023-01-20 14:18:49 +0100

    hackernews_fix

--- src/hackernews.rs
+++ src/hackernews.rs
@@ -39,6 +39,56 @@ fn deserialize_hackernews_story(json_str: String) -> HackernewsStruct {
     }
 }
 
+pub fn hacker_news() {
+    let client = get_client();
+
+    let story_urls = get_all_id(&client);
+
+    get_all_stories(client, story_urls);
+}
+
+fn get_client() -> reqwest::blocking::Client {
+    reqwest::blocking::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .unwrap()
+}
+
+fn get_all_stories(client: reqwest::blocking::Client, story_urls: Vec<String>) {
+    for url in story_urls {
+        let response = client.get(url).send().unwrap().text().unwrap();
+
+        let response = deserialize_hackernews_story(response);
+
+        let link_string = format!(
+            "\x1B]8;;{}\x07{}\x1B]8;;\x07",
+            &response.url.unwrap_or("default_url".to_string()),
+            &response.title
+        );
+        println!("{}", link_string.red());
+    }
+}
+
+fn get_all_id(client: &reqwest::blocking::Client) -> Vec<String> {
+    let response = client
+        .get("https://hacker-news.firebaseio.com/v0/topstories.json")
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let response = deserialize_hackernews_top_stories(response);
+
+    response
+        .0
+        .into_iter()
+        .map(|x| {
+            "https://hacker-news.firebaseio.com/v0/item/".to_owned() + &x.to_string() + ".json"
+        })
+        .collect()
+}
+
+//chat-gpt recommends:
 // impl<T> T where T: serde::de::DeserializeOwned {
 //     fn from_json(json_str: String) -> Self {
 //         match serde_json::from_str(&json_str) {
@@ -53,44 +103,3 @@ fn deserialize_hackernews_story(json_str: String) -> HackernewsStruct {
 
 // let top_stories = HackernewsTopstoriesStruct::from_json(json_string);
 // let story = HackernewsStruct::from_json(json_string);
-
-pub fn hacker_news() -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::blocking::Client::builder()
-        .redirect(reqwest::redirect::Policy::none())
-        .build()
-        .unwrap();
-    let res = client
-        .get("https://hacker-news.firebaseio.com/v0/topstories.json")
-        .send()?
-        .text()?;
-    //println!("{}", res);
-    let response = deserialize_hackernews_top_stories(res);
-    //let num: hackernews_topstories_struct = serde_json::from_str(&res).unwrap();
-    //println!("{:?}", response.0[5]);
-
-    /// Testing this
-    let res: Vec<String> = response
-        .0
-        .into_iter()
-        .map(|x| {
-            "https://hacker-news.firebaseio.com/v0/item/".to_owned() + &x.to_string() + ".json"
-        })
-        .collect();
-
-    /// End of testing here
-    /// The below code works tho
-    for i in res {
-        // let url: String =
-        //     "https://hacker-news.firebaseio.com/v0/item/".to_owned() + &i.to_string() + ".json";
-        let res = client.get(i).send()?.text()?;
-        let response = deserialize_hackernews_story(res);
-        //println!("{}\n", response.url?);
-        let link_string = format!(
-            "\x1B]8;;{}\x07{}\x1B]8;;\x07",
-            &response.url.unwrap_or("default_url".to_string()),
-            &response.title
-        );
-        println!("{}", link_string.red());
-    }
-    Ok(())
-}
